079c24626886710481edf674f3029e8cd60937e7
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-10-06 20:14:11 -0400

    updated gitignore

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const mongoose = require('mongoose');
-mongoose
-.connect(`mongodb+srv://Dillon:<password>@cluster0.r0lrf.mongodb.net/<dbname>?retryWrites=true&w=majority`,
-{ useNewUrlParser: true, useUnifiedTopology: true })
- .then(() => console.log('Connected to MongoDB...'))
- .catch((err) => console.log(`Could not connect to MongoDB. ERROR: ${err}`));
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
+});
