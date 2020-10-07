827bf872909e0555a3eec8b2d873e69b0dca16fd
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-10-07 18:12:34 -0400

    moved db.js to startup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db');
+const connectDB = require('./startup/db');
 const app = express();
 const videos = require('./routes/videos');
 
