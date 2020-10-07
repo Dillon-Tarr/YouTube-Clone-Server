a8d4988fb21a11d2704ac50656db4e4b2dedf6d4
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-10-07 19:47:03 -0400

    added get requests for all videos and one video by ID

--- routes/videos.js
+++ routes/videos.js
@@ -3,6 +3,26 @@ const {Video, validateVideo, Comment, Reply} = require('../models/video-Schema')
 const express = require('express');
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  try {
+  const videos = await Video.find();
+  return res.send(videos);
+  } catch (ex) {
+  return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+}); 
+
+router.get('/:id', async (req, res) => {
+  try {
+  const video = await Video.findById(req.params.id);
+  if (!video)
+  return res.status(400).send(`The video with id "${req.params.id}" does not exist.`);
+  return res.send(video);
+  } catch (ex) {
+  return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+}); 
+
 router.post('/', async (req, res) => {
   try {
   const { error } = validateVideo(req.body);
