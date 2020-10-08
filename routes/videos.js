c65a4b8d3097156769e90b7565095d53644b92cf
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-10-07 20:21:51 -0400

    added put router for updating likes and dislikes

--- routes/videos.js
+++ routes/videos.js
@@ -3,6 +3,7 @@ const {Video, validateVideo, Comment, Reply} = require('../models/video-Schema')
 const express = require('express');
 const router = express.Router();
 
+//Get all videos
 router.get('/', async (req, res) => {
   try {
   const videos = await Video.find();
@@ -12,6 +13,7 @@ router.get('/', async (req, res) => {
   }
 }); 
 
+//Get a video by ID
 router.get('/:id', async (req, res) => {
   try {
   const video = await Video.findById(req.params.id);
@@ -23,13 +25,19 @@ router.get('/:id', async (req, res) => {
   }
 }); 
 
+//Add a video
 router.post('/', async (req, res) => {
   try {
   const { error } = validateVideo(req.body);
   if (error)
   return res.status(400).send(error);
 
-  const video = new Video(req.body);
+  const video = new Video({
+    videoId: req.body.videoId,
+    likes: req.body.likes,
+    dislikes: req.body.dislikes,
+    comments: req.body.comments
+    });
   await video.save();
   return res.send(video);
   } catch (ex) {
@@ -37,5 +45,27 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Update LIKES or DISLIKES
+router.put('/:id', async (req, res) => {
+  try {
+    const { error } = validateVideo(req.body);
+  if (error) return res.status(400).send(error);
+  const video = await Video.findByIdAndUpdate(
+    req.params.id,
+    {
+    likes: req.body.likes,
+    dislikes: req.body.dislikes,
+    },
+    { new: true }
+  );
+  if (!video)
+    return res.status(400).send(`The video with id "${req.params.id}" does not exist.`);
+  await video.save();
+  return res.send(video);
+  } catch (ex) {
+  return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
+ 
 
 module.exports = router;
