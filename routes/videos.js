32dcd3c348208a467103ebc1831d0e0af5027477
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-10-07 21:25:20 -0400

    added requests to add comments and replies

--- routes/videos.js
+++ routes/videos.js
@@ -48,8 +48,6 @@ router.post('/', async (req, res) => {
 // Update LIKES or DISLIKES
 router.put('/:id', async (req, res) => {
   try {
-    const { error } = validateVideo(req.body);
-  if (error) return res.status(400).send(error);
   const video = await Video.findByIdAndUpdate(
     req.params.id,
     {
@@ -66,6 +64,36 @@ router.put('/:id', async (req, res) => {
   return res.status(500).send(`Internal Server Error: ${ex}`);
   }
 });
- 
+
+//Add a comment to an existing video
+router.put('/add_comment/:id', async (req, res) => {
+  try {
+  const video = await Video.findById(req.params.id);
+  if (!video)
+    return res.status(400).send(`The video with id "${req.params.id}" does not exist.`);
+  video.comments.push(req.body);
+  await video.save();
+  return res.send(video);
+  } catch (ex) {
+  return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
+
+//Add a reply to an existing comment
+router.put('/:videoId/add_reply/:commentId', async (req, res) => {
+  try {
+  const video = await Video.findById(req.params.videoId);
+  if (!video)
+    return res.status(400).send(`The video with id "${req.params.videoId}" does not exist.`);
+  const comment = video.comments.id(req.params.commentId);
+  if (!comment)
+    return res.status(400).send(`The comment with id "${req.params.commentId}" does not exist.`);
+  comment.replies.push(req.body);
+  await video.save();
+  return res.send(video);
+  } catch (ex) {
+  return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
 
 module.exports = router;
